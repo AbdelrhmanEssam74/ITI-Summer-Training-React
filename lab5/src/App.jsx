e371cea9c18a965ce0445b8a1cedd7e12e01800c
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-06 02:22:56 +0300

    Add initial Bootstrap-based layout and hero section
    - Create `Hero` component for the landing page.
    - Update `App` component to include `Hero`.
    - Introduce CSS styles for layout and background images.
    - Remove unused code and logos from the `App` component.

---
+++ lab5/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Hero from "./Components/Hero-section/Hero.jsx";
+
+function App() {
+    return (
+        <>
+                <Hero/>
+
+        </>
+    )
+}
+
+export default App
