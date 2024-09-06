9b9b5662b37b7e8e66f321a6df545b58ed29c2a0
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-06 03:57:57 +0300

    Create initial HTML structure for portfolio page in index.html

--- lab5/src/App.jsx
+++ lab5/src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Hero from "./Components/Hero-section/Hero.jsx";
