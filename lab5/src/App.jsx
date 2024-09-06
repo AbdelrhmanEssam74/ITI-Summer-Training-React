7c99c2971d1757ceba3d27f803eb3b858745e529
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-06 03:37:51 +0300

    Add Portfolio component and style updates
    - Create new Portfolio component in `Portfolio.jsx`
    - Import and render the Portfolio component in `App.jsx`
    - Update CSS in `App.css` for portfolio layout and widget styles
    - Fix typo in the component name from `Portolio` to `Portfolio`

--- lab5/src/App.jsx
+++ lab5/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Hero from "./Components/Hero-section/Hero.jsx";
 import About from "./Components/about/About.jsx";
 import Skills from "./Components/skills/Skills.jsx";
+import Portfolio from "./Components/portfolio/Portfolio.jsx";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Hero/>
             <About/>
             <Skills/>
+            <Portfolio/>
         </>
     )
 }
